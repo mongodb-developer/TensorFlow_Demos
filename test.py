9c985f68382283b90c1fffb0ad273624f5f8e941
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-04 13:26:48 -0500

    Create test.py
    The code fetches a movie document from the sample movies collection in MongoDB, and in this case, it retrieves a document with the title "Blacksmith Scene". The code then performs a simple TensorFlow operation to add the constants 5 and 10, which results in 15. Tests to make sure Tensorflow is working properly

---
+++ test.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import pymongo
+
+# MongoDB connection string
+connection_string = 'mongodb+srv://jschmitz:slb2021@cisco-demo.tnhx6.mongodb.net/sample_mflix'
+
+# Connect to MongoDB
+client = pymongo.MongoClient(connection_string)
+
+# Access the movies collection
+db = client.sample_mflix
+movies_collection = db.movies
+
+# Fetch a movie document
+movie = movies_collection.find_one()
+
+# Print the movie title
+print("Movie Title:", movie['title'])
+
+# Perform a simple TensorFlow operation
+a = tf.constant(5)
+b = tf.constant(10)
+c = tf.add(a, b)
+
+# Print the result
+print("Result:", c.numpy())
